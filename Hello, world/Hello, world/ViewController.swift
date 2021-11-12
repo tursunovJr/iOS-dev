a02b1b7f573556620de612a50abffdf06b63efdd
Jasur Tursunov <jasurtursunov@MacBook-Air-Jasur.local>
2021-11-13 00:18:34 +0300

    hello world add

---
+++ Hello, world/Hello, world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello, world
+//
+//  Created by Jasur Tursunov on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
